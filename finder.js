3e45b7572bf1d5f790b52c50782e1a6c810a7edd
gobica <alkaz.gobec@gmail.com>
2021-06-10 12:28:17 +0200

    popraveki

--- finder.js
+++ finder.js
@@ -26,7 +26,7 @@ AFRAME.registerComponent('finder', {
         var scene = document.querySelector('a-scene');
 
         console.log(`https://cors.bridged.cc/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&type=tourist_attraction&rankby=distance&radius=2000&key=AIzaSyCC2aDWxhRGLni1Tz5MlhdX9-6WwX5d3kM`);
-        fetch(`https://cors.bridged.cc/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&type=tourist_attraction&rankby=distance&radius=2000&key=AIzaSyCC2aDWxhRGLni1Tz5MlhdX9-6WwX5d3kM`)
+        fetch(`https://cors.bridged.cc/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&type=tourist_attraction&rankby=distance&key=AIzaSyCC2aDWxhRGLni1Tz5MlhdX9-6WwX5d3kM`)
             .then(function (response) {
 
                 if (response.status !== 200) {
